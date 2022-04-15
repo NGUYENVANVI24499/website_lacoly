2d8a0dc054598b477fa846ebfa44aa81eb31a470
Nguyễn Văn Vĩ <43158136+NGUYENVANVI24499@users.noreply.github.com>
2022-04-15 09:47:36 +0700

    first commit

---
+++ assets/js/slide.js
@@ -0,0 +1,22 @@
+$('.categoryproduct-image_left').slick({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    utoplaySpeed:true,
+    autoplay:true,
+    arrows:false,
+    autoplaySpeed:1000,
+    
+  });
+  
+
+  $('.news-list').slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    utoplaySpeed:true,
+    autoplay:true,
+    arrows:false,
+    autoplaySpeed:2000,
+  });
+  
